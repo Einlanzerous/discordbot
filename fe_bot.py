fb859dddd35fd0f07a83ef6588631ec179def821
Einlanzerous <einlanzerous@live.com>
2017-08-08 21:09:39 -0400

    Adding itms

--- fe_bot.py
+++ fe_bot.py
@@ -1,10 +1,10 @@
 from random import randint
 import random
+import os
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
-
 
 def roll_d6():
     outcome = randint(1, 6)
@@ -61,7 +61,52 @@ async def on_message(message):
             await client.send_message(message.channel, fmt)
             return
         if int(res.content) == 1:
-            await client.send_message(message.channel, "Res level 1")
+            points = 1
+
+            variantN = randint(0, 10)
+            shellsN = randint(0, 10)
+            variant = ""
+            shells = ""
+
+            if (variantN == 0):
+                variant = "Burst Spores"
+            elif (variantN == 1):
+                variant = "Gauss"
+            elif (variantN == 2):
+                variant = "Ion"
+            elif (variantN == 3):
+                variant = "Irradiated"
+            elif (variantN == 4):
+                variant = "Particle"
+            elif (variantN == 5):
+                variant = "Rail"
+            elif (variantN == 6):
+                variant = "Self-Propelled"
+            elif (variantN == 7):
+                variant = "Spine Launcher"
+            elif (variantN >= 8):
+                variant = "Standard"
+
+            if (shellsN == 0):
+                shells = "Dispersion"
+            elif (shellsN == 1):
+                shells = "Dummy"
+            elif (shellsN == 2):
+                shells = "Electro-Gravity"
+            elif (shellsN == 3):
+                shells = "Kinetic"
+            elif (shellsN == 4):
+                shells = "Shrapnel"
+            elif (shellsN == 5):
+                shells = "Smoke"
+            elif (shellsN == 6):
+                shells = "Snare"
+            elif (shellsN >= 7):
+                shells = "Normal"
+
+
+
+            await client.send_message(message.channel, "Pistol; with " + variant + " variation and " + shells + " shells.")
         elif int(res.content) == 2:
             await client.send_message(message.channel, "res level 2")
         else:
@@ -73,4 +118,4 @@ async def loot(res: int):
     await client.say(res)
 
 
-client.run("MzQxOTk1MzEwNzY5NTA0MjU2.DGTuvA.1tNZ_WOXrfpvtzlp6ygK1sZQZW0")
+client.run(os.environ["DiscordBot"])
